98b033c74a62bc5a4e0ea4ebbaca7af7703d5db1
Gayathrir1666 <gayathrir9911@gmail.com>
2017-07-22 00:43:25 +0530

    layout

---
+++ java/rg/com/cb1/cb1.java
@@ -0,0 +1,17 @@
+package rg.com.cb1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class cb1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cb1);
+
+    }
+}
